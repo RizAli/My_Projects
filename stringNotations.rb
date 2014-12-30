0f0cb1d0c34a8984504a8a999577f839bbe6d171
Rizwan Ali <rizcube@gmail.com>
2014-12-30 07:37:54 +0000

    added number.rb

---
+++ stringNotations.rb
@@ -0,0 +1,34 @@
+puts "This is a string manipulation excercises"
+puts "Two plus two is #{ 2 + 2}"
+puts 'Two plus two is #{ 2 + 2}'
+
+puts "#{2 + 2 }"
+puts '#{2 + 2 }'
+
+string = "Ruby is a cool language."
+puts string[5]
+puts string [-14..-1]
+puts string [0,10]
+
+puts "a" + "b"
+puts "a" + "b" + "c"
+
+str = "Hi"
+puts str + " there"
+puts str
+#  =========================================================
+
+a = "Hello there"
+puts a[1]
+puts a[2,3]
+puts a[2..3]
+puts a[-5,5]
+
+puts a[0..-2]
+puts a[-4..-2]
+puts a[-2..-1]
+
+
+
+
+
